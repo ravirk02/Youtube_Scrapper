001bd5d6d63b040a0d506d132fbd5f04880b152e
Ravi kashyap <148521282+ravirk02@users.noreply.github.com>
2024-12-08 14:43:19 +0530

    Add files via upload

---
+++ Youtube_Scrapper/thumbnail_retrieve.py
@@ -0,0 +1,31 @@
+import os
+import requests
+
+
+def persist_image(folder_path,thumbnail_url):
+
+    url = thumbnail_url
+    try:
+        if not os.path.exists(folder_path):
+            os.chdir(os.path.join(os.getcwd(), folder_path))
+            os.mkdir(os.path.join(os.getcwd(), folder_path))
+    except:
+        pass
+
+    
+    try:
+        image_content = requests.get(url).content
+
+    except Exception as e:
+        print(f"ERROR - Could not download {url} - {e}")
+  
+
+    try:
+        f = open(os.path.join(folder_path + ".jpg" ), 'wb')
+        f.write(image_content)
+        f.close()
+        print(f"SUCCESS - saved {url} - as Image + '/' ")
+    except Exception as e:
+        print(f"ERROR - Could not save {url} - {e}")
+
+persist_image('thumbnails',thumbnail_url)
